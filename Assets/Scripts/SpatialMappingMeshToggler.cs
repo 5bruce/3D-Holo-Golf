ac08ab4b2ff95ca24f2cbd048b85386036875a64
reedv <villanuevareed@gmail.com>
2017-02-07 01:51:56 -1000

    Toggle spatial mesh done. Cursor-behind-ball bug seems fixed.
    Decreased min cursor distance from 1 to 0.45 to try to fix cursor-not-on-ball bug. The idea being that maybe the cursor needs to have a min. distance < forwardOffset of the projectile.

---
+++ Assets/Scripts/SpatialMappingMeshToggler.cs
@@ -0,0 +1,38 @@
+﻿using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Toggles spatial mesh drawing.
+/// Intended to be activated/deactiveated by KeywordManager
+/// </summary>
+public class SpatialMappingMeshToggler : MonoBehaviour {
+
+    /// <summary>
+    /// Controls spatial mapping.  In this script we access spatialMappingManager
+    /// to control rendering and to access the physics layer mask.
+    /// </summary>
+    protected SpatialMappingManager spatialMappingManager;
+
+    // Use this for initialization
+    void Start () {
+        spatialMappingManager = SpatialMappingManager.Instance;
+        if (spatialMappingManager == null)
+        {
+            Debug.LogError("This script expects that you have a SpatialMappingManager component in your scene.");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToggleMesh()
+    {
+        Debug.Log("SpatialMappingToggler: ToggleMesh() called");
+        if (spatialMappingManager.DrawVisualMeshes) spatialMappingManager.DrawVisualMeshes = false;
+        else spatialMappingManager.DrawVisualMeshes = true;
+    }
+}
