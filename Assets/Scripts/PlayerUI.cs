fddc8449b737f54e0285a6316425f9a37a46f223
reedv <villanuevareed@gmail.com>
2017-02-07 17:20:21 -1000

    created basic UI using conventional unity UI text. gets hidden behind spatial mapping. need to convert to HoloToolkit UI

---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+/// <summary>
+/// Displays information to player's HUD.
+/// Receives messages from ProjectileShooter.
+/// </summary>
+public class PlayerUI : MonoBehaviour {
+    /// <summary>
+    /// UI Text component to write stroke count to
+    /// </summary>
+    public Text strokesText;
+
+    public Text goalText;
+
+	// Use this for initialization
+	void Start () {
+        strokesText.text = "Strokes: 0";
+        goalText.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void ProjectileLaunched(int strokes)
+    {
+        Debug.Log("PlayerUI: ProjectileLaunched()");
+        strokesText.text = "Strokes: " + strokes;
+    }
+
+    void GoalEntered(int strokes)
+    {
+        Debug.Log("PlauerUI: GoalEntered()");
+        if (strokes == 1) goalText.text = "Hole in one!";
+        else goalText.text = "Finished in " + strokes + " strokes!";
+    }
+}
--- Assets/Scripts/ProjectileShooter.cs
+++ Assets/Scripts/ProjectileShooter.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
 /// <summary>
-/// Controls whether the attached-to projectile is launching or resting
+/// Controls the launching and reseting of the attached-to projectile gameobject
 /// </summary>
 public class ProjectileShooter : MonoBehaviour {
-    //public GameObject projectile_prefab;
+    /// <summary>
+    /// Reference this projectile launches towards when relaeased from handdragging
+    /// </summary>
     public GameObject reference_point;
+    
+    /// <summary>
+    /// Trigger object that counts as this projectile's goal trigger
+    /// </summary>
+    public GameObject goal;
 
     public float forwardOffset = 0.5f;
     public const float throwRatio = 10.0f;
     public bool resting;
 
+    int strokes;
+
     // Use this for initialization
     void Start () {
+        strokes = 0;
         resting = true;
         gameObject.GetComponent<Rigidbody>().useGravity = false;
 
@@ -40,6 +50,17 @@ public class ProjectileShooter : MonoBehaviour {
         resting = true;
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ProjectileShooter: OnTriggerEntered()");
+        if(other.transform.gameObject.name == goal.name)
+        {
+            Debug.Log("ProjectileShooter: goal entered");
+            gameObject.SetActive(false);  // can't be activated once inactive (just for initial testing)
+            SendMessageUpwards("GoalEntered", strokes);
+        }
+    }
+
     /// <summary>
     /// Prepares projectile to be dragged from resting position
     /// </summary>
@@ -61,5 +82,8 @@ public class ProjectileShooter : MonoBehaviour {
         Vector3 direction = reference_point.transform.position - gameObject.transform.position;
         float magnitude = direction.magnitude * throwRatio;
         rb.velocity = direction * magnitude;
+
+        strokes++;
+        SendMessageUpwards("ProjectileLaunched", strokes);
     }
 }
