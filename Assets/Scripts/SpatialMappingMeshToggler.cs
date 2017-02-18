37a5b0d93c1166178e7903d91281dbca41511f6b
reedv <villanuevareed@gmail.com>
2017-02-17 23:47:12 -1000

    Added spatial understanding manager for surfacetoplane processing and timed scanning
    Surface planes respond to "Toggle Mesh" voice command.
    Added more triangles per area in spatial mesh for smoother mesh.

--- Assets/Scripts/SpatialMappingMeshToggler.cs
+++ Assets/Scripts/SpatialMappingMeshToggler.cs
@@ -1,4 +1,5 @@
-﻿using HoloToolkit.Unity.SpatialMapping;
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.SpatialMapping;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,9 +16,15 @@ public class SpatialMappingMeshToggler : MonoBehaviour {
     /// </summary>
     protected SpatialMappingManager spatialMappingManager;
 
+    /// <summary>
+    /// If exists in scene, controls creation of planes from suface mesh
+    /// </summary>
+    protected SurfaceMeshesToPlanes surfaceMeshesToPlanes;
+
     // Use this for initialization
     void Start () {
         spatialMappingManager = SpatialMappingManager.Instance;
+        surfaceMeshesToPlanes = SurfaceMeshesToPlanes.Instance;
         if (spatialMappingManager == null)
         {
             Debug.LogError("This script expects that you have a SpatialMappingManager component in your scene.");
@@ -32,7 +39,24 @@ public class SpatialMappingMeshToggler : MonoBehaviour {
     public void ToggleMesh()
     {
         Debug.Log("SpatialMappingToggler: ToggleMesh() called");
+        // toggle rendering of spatial mesh
         if (spatialMappingManager.DrawVisualMeshes) spatialMappingManager.DrawVisualMeshes = false;
         else spatialMappingManager.DrawVisualMeshes = true;
+
+        // toggle rendering of surface planes
+        if(surfaceMeshesToPlanes != null)
+        {
+            List<GameObject> activePlanes = surfaceMeshesToPlanes.ActivePlanes;
+            if (activePlanes[0].GetComponent<MeshRenderer>().enabled)
+            {
+                foreach (GameObject plane in activePlanes)
+                    plane.GetComponent<MeshRenderer>().enabled = false;
+            }
+            else
+            {
+                foreach (GameObject plane in activePlanes)
+                    plane.GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
     }
 }
