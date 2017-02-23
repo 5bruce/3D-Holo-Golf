35930dd0dfcf104473fd1286e7a65a55b5cc6ed6
Knight125 <karanicoleknight@gmail.com>
2017-02-22 21:38:45 -1000

    Kara's Menu Mostly Works
    It works right now, but I need to change on click to something the hololens user can select. I might have to revamp it.

---
+++ Assets/AddObjectMenuAssets/clickHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickHandler : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject myPrefab1;
+    public GameObject panel;
+    public int numPlayers = 1;
+    private int objectsCreated = 0;
+
+    public void button_click(GameObject generate)
+    {
+        GameObject createdObject;
+        createdObject = (GameObject)Instantiate(generate);
+        objectsCreated += 1;
+        //set draggable
+        if (objectsCreated == numPlayers)
+        {
+            //move on to next round
+            panel.SetActive(false);
+            //randomize menu with 5 obstacles to choose from
+            objectsCreated = 0;
+        }
+    }
+}
