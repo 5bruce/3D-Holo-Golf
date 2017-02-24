23512dd0db777dea5e12f821c620eb59665744ea
reedv <villanuevareed@gmail.com>
2017-02-23 21:06:31 -1000

    added ObstacleMenuManager w/ debugging component to Managers
    Can now active obstacleMenu for temporary debuggung purposes using voice command (see InputManager component)

---
+++ Assets/Scripts/ObstacleMenuDebugger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMenuDebugger : MonoBehaviour {
+    /// <summary>
+    /// The obstacle menu gameobject that this component is intended to do debugging for
+    /// </summary>
+    public GameObject ObstacleMenu;
+
+    bool menuActive;
+
+	// Use this for initialization
+	void Start () {
+        // initially want this menu hidden
+        ObstacleMenu.SetActive(false);
+        menuActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// toggles whether the obstaclemenu gameobject is active 
+    /// </summary>
+    public void EnableMenu()
+    {
+        Debug.Log("ObstacleMenuManager:ObstacleMenuDebugger: EnableMenu()");
+        ObstacleMenu.SetActive(!menuActive);
+        menuActive = !menuActive;
+    }
+}
