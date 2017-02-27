870a7c4b7996318a0d4abb1fe4e174c977c1782b
Knight125 <karanicoleknight@gmail.com>
2017-02-26 15:50:37 -1000

    Button click works
    Button Click works but a lot of things need to be fixed. It is two close
    to the screen and the texture on the object button does not want to show
    up. Also the created object needs to be draggable.

--- Assets/AddObjectMenuAssets/clickHandler.cs
+++ Assets/AddObjectMenuAssets/clickHandler.cs
@@ -1,19 +1,50 @@
-﻿using System.Collections;
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class clickHandler : MonoBehaviour {
-
-    // Use this for initialization
-    public GameObject myPrefab1;
+public class clickHandler : MonoBehaviour,
+                                 IFocusable,
+                                 IInputClickHandler
+{
+    public Material active_material;
+    public Material inactive_material;
+    public GameObject selectionObject;
     public GameObject panel;
     public int numPlayers = 1;
     private int objectsCreated = 0;
 
-    public void button_click(GameObject generate)
+    // Use this for initialization
+    void Start()
     {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void IFocusable.OnFocusEnter()
+    {
+        Debug.Log("ExitButtonHandler: OnFocusEnter()");
+        gameObject.GetComponent<MeshRenderer>().material = active_material;
+    }
+
+    void IFocusable.OnFocusExit()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = inactive_material;
+    }
+
+    void IInputClickHandler.OnInputClicked(InputClickedEventData eventData)
+    {
+        //on click function
+        Debug.Log("ObjectButtons: OnInputClicked()");
+
         GameObject createdObject;
-        createdObject = (GameObject)Instantiate(generate);
+        createdObject = (GameObject)Instantiate(selectionObject);
         objectsCreated += 1;
         //set draggable
         if (objectsCreated == numPlayers)
