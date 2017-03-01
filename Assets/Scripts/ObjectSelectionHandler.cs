6a256819f814fa85e3c05d25fa806957749261a7
Knight125 <karanicoleknight@gmail.com>
2017-02-28 19:54:35 -1000

    Working on Game Menu Handler

---
+++ Assets/Scripts/ObjectSelectionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObjectSelectionHandler : MonoBehaviour {
+    public GameObject canvas;
+    public GameObject[] prefabs = new GameObject[2];
+
+    void instantiateGameObjectMenu()
+    {
+        for(int i = 1; i <= prefabs.Length;i++)
+        {
+            instantiateObj(prefabs[randomNumberSelector(1,5)]);
+        }
+    } 
+
+
+    int randomNumberSelector(int min, int max)
+    {
+        System.Random rnd = new System.Random();
+        return rnd.Next(min, max);
+    }
+    
+    void instantiateObj(GameObject obj)
+    {
+        //create button
+    }
+}
