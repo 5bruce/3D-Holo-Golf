573e9f790d9dede1a5daf0464b1f65f39fe91d96
reedv <villanuevareed@gmail.com>
2017-03-02 00:59:41 -1000

    fixed PlayAndPassManager null exception list bug and wrong child bug

--- Assets/Scripts/PlayAndPassManager.cs
+++ Assets/Scripts/PlayAndPassManager.cs
@@ -10,36 +10,48 @@ public class PlayAndPassManager : MonoBehaviour {
     /// <summary>
     /// Prefab or gameobject to use as tempate for generating players
     /// </summary>
-    public GameObject playerPrefab;
+    public GameObject firstPlayer;
 
     SettingsManager settingsAccess;
     int numberOfPlayers;
-    List<GameObject> players;
+    List<GameObject> players = new List<GameObject>();
+    GameObject activePlayer;
 
 	// Use this for initialization
 	void Start () {
-        if (!playerPrefab)
+        if (!firstPlayer)
         {
             Debug.LogError(this.name + ": no player prefab assigned");
         }
         else
         {
+            // ensure that the firstPlayer is active and first to play
+            if (firstPlayer.activeSelf == false)
+            {
+                firstPlayer.SetActive(true);
+            }
+            activePlayer = firstPlayer;
+
             // get access to settings information
             settingsAccess = GameObject.Find("SettingsManager").GetComponent<SettingsManager>();
 
+            // get number of player for this game
             numberOfPlayers = settingsAccess.numberOfPlayers;
             Debug.Log(this.name + ": numberOfPlayers = " + numberOfPlayers);
 
-            // create list of Player gameobject children and instantiate them
-            Debug.Log(this.name + ": creating players");
-            for (int i = 0; i < numberOfPlayers; i++)
-            {
-                GameObject player = Instantiate(playerPrefab, playerPrefab.transform);
-                Debug.Log(this.name + "player clone: " + player.name);
-                players.Add(player);  // FIXME: raising null exception
-            }
+            // add the first player to players list (there should always be a first player)
+            players.Add(firstPlayer);
 
-            // set the first child Player in the list to be the current active
+            // create list of other Player gameobject children and instantiate them
+            Debug.Log(this.name + ": creating players");
+            for (int i = 1; i < numberOfPlayers; i++)
+            {
+                GameObject player = Instantiate(firstPlayer, firstPlayer.transform.position, firstPlayer.transform.rotation, gameObject.transform);
+                player.name = "Player" + (1+i);
+                player.SetActive(false);
+                Debug.Log(this.name + ": player clone: " + player.name);
+                players.Add(player); 
+            }
         }
 	}
 	
