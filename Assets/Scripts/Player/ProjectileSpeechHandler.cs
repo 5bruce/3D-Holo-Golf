e042e3af782ee65dfe3bbf04250a1512fb54641b
reedv <villanuevareed@gmail.com>
2017-03-05 20:34:00 -1000

    PlayAndPass: can switch between player control by activating/deactivatin gameobject
    Currently connot see other players' balls, when switch from one to another; need to change this

--- Assets/Scripts/Player/ProjectileSpeechHandler.cs
+++ Assets/Scripts/Player/ProjectileSpeechHandler.cs
@@ -7,12 +7,13 @@ using System;
 public class ProjectileSpeechHandler : MonoBehaviour, 
                                        ISpeechHandler
 {
-    // FIXME: I'm not sure that this is properly implementing the interface
-    public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData)
+    // ISpeechHandler only works on object that are in focus/gazed on
+    // Need to attach SetGlobalListener.cs component as well to have global listening
+    void ISpeechHandler.OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData)
     {
-        if(eventData.RecognizedText == "Reset Ball")
+        Debug.Log(gameObject.name + ": " + this.GetType().Name + ": key word = " + eventData.RecognizedText);
+        if (eventData.RecognizedText == "Reset Ball")
         {
-            Debug.Log("ProjectileSpeechHandler 'Reset Ball' kword recognized");
             SendMessageUpwards("OnReset");
         }
     }
---
+++ Assets/Scripts/SetGlobalListener.cs
@@ -0,0 +1,20 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Register this game object on the InputManager as a global listener.
+/// </summary>
+public class SetGlobalListener : MonoBehaviour
+{
+    private void Start()
+    {
+        InputManager.Instance.AddGlobalListener(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        InputManager.Instance.RemoveGlobalListener(gameObject);
+    }
+}
