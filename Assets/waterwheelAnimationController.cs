29891ae352c9bd85ea7bb0acc77148eddb61c258
Knight125 <karanicoleknight@gmail.com>
2017-03-14 23:23:42 -1000

    WaterWheel Works

---
+++ Assets/waterwheelAnimationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterwheelAnimationController : MonoBehaviour {
+    public Animation anim;
+	// Use this for initialization
+	void Start () {
+        anim["Waterwheel3Animation"].speed = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
