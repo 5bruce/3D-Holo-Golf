c0adaf4f74c17f761b652ec36e81db713826bce8
reedv <villanuevareed@gmail.com>
2017-03-16 23:07:50 -1000

    Added a rotating cog w/ rotating paddles using unity 3D models

---
+++ Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour {
+
+    public float speed = 20f;
+
+    void LateUpdate()
+    {
+        transform.Rotate(Vector3.right, speed * Time.deltaTime);
+    }
+}
+
